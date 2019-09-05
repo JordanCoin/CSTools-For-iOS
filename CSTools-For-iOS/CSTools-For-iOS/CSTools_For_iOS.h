e4969c20ea27d83e9a11094834ed706d5e4991ce
Jordan Coin Jackson <jwilliamcj29@gmail.com>
2019-09-04 22:25:53 -0400

    created framework

---
+++ CSTools-For-iOS/CSTools-For-iOS/CSTools_For_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CSTools_For_iOS.h
+//  CSTools-For-iOS
+//
+//  Created by Jordan Jackson on 9/4/19.
+//  Copyright © 2019 CodeSigned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CSTools_For_iOS.
+FOUNDATION_EXPORT double CSTools_For_iOSVersionNumber;
+
+//! Project version string for CSTools_For_iOS.
+FOUNDATION_EXPORT const unsigned char CSTools_For_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSTools_For_iOS/PublicHeader.h>
+
+
